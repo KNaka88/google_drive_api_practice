b0e841f98b9c1db5e485926f973bf7bf38ba939a
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-21 12:44:04 -0700

    create google drive api practice file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/./vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfig('client_secrets.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+    $client->setAccessToken($_SESSION['access_token']);
+    $drive = new Google_Service_Drive($client);
+    $files = $drive->files->listFiles(array())->getFiles();
+    echo json_encode($files);
+} else {
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+    header('Location: ' . $redirect_url);
+}
+
+?>
