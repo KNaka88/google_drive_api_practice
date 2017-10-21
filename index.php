8044e418f5447df69bcec70c43420211a6e4f40a
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-21 14:26:23 -0700

    fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/./vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 session_start();
 
@@ -8,13 +8,11 @@ $client->setAuthConfig('client_secrets.json');
 $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-    $client->setAccessToken($_SESSION['access_token']);
-    $drive = new Google_Service_Drive($client);
-    $files = $drive->files->listFiles(array())->getFiles();
-    echo json_encode($files);
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive = new Google_Service_Drive($client);
+  $files = $drive->files->listFiles(array())->getFiles();
+  echo json_encode($files);
 } else {
-    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
-    header('Location: ' . $redirect_url);
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
-
-?>
